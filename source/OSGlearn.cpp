e7e8aff1c2704146aeb75f74cbe302fad258b6c0
无名 无姓 <lsj2543075618@gmail.com>
2023-05-25 11:21:05 +0800

    添加项目文件。

---
+++ source/OSGlearn.cpp
@@ -0,0 +1,32 @@
+﻿// OSGlearn.cpp: 定义应用程序的入口点。
+//
+
+#include "OSGlearn.h"
+#include<iostream>
+#include <osgViewer/Viewer>
+#include <osg/Geode>
+#include <osg/ShapeDrawable>
+#include <osg/Texture2D>
+#include <osgDB/ReadFile>
+
+using namespace std;
+
+int main()
+{
+	std::cout << "Hello,osg!" << std::endl;
+	osg::ref_ptr<osgViewer::Viewer> viewer = new osgViewer::Viewer;
+	viewer->setUpViewInWindow(50,50,800,600);
+
+	// 创建球体
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	geode->addDrawable(new osg::ShapeDrawable(new osg::Sphere(osg::Vec3(),1.0f)));
+
+	// 给球体添加纹理
+	osg::ref_ptr<osg::Texture> texture = new osg::Texture2D(osgDB::readRefImageFile("texture/world.png"));//需要设置OSG的文件搜索路径
+	auto stateSet = geode->getOrCreateStateSet();
+	stateSet->setTextureAttributeAndModes(0,texture);
+
+	viewer->setSceneData(geode);
+
+	return viewer->run();
+}
