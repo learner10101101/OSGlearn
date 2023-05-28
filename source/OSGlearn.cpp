08172e2402b4667ebd151c94ac86b9b79a9628f2
无名 无姓 <lsj2543075618@gmail.com>
2023-05-28 23:49:24 +0800

    添加设置属性的场景，未获得源码，可绘制节点不正确，案例无法正常运行

--- source/OSGlearn.cpp
+++ source/OSGlearn.cpp
@@ -13,6 +13,13 @@
 #include <osgDB/Registry>
 #include <osgDB/WriteFile>
 #include <osg/Notify>
+#include <osg/StateSet>
+#include <osg/StateAttribute>
+#include <osg/ShadeModel>
+#include <osg/CullFace>
+#include <osg/PolygonMode>
+#include <osg/LineWidth>
+#include <osg/MatrixTransform>
 
 // 绘制地球
 osg::ref_ptr<osg::Node> createSceneGraph1()
@@ -67,6 +74,127 @@ osg::ref_ptr<osg::Node> createSceneGraph2()
 	return geode.get();
 }
 
+// 修改渲染状态
+osg::ref_ptr<osg::Geometry> createDrawable()// 这个Geometry与案例不符
+{
+	osg::ref_ptr<osg::Geometry> geom = new osg::Geometry();
+
+	// 创建四个顶点的数组
+	osg::ref_ptr<osg::Vec3Array> v = new osg::Vec3Array;
+	geom->setVertexArray(v.get());// 注意，这里v是指针，但是用“.”来使用get()函数返回指针
+	v->push_back(osg::Vec3(-1.f, 0.f, -1.f));
+	v->push_back(osg::Vec3(1.f, 0.f, -1.f));
+	v->push_back(osg::Vec3(1.f, 0.f, 1.f));
+	v->push_back(osg::Vec3(-1.f, 0.f, 1.f));
+
+	// 创建四种颜色的数组
+	osg::ref_ptr<osg::Vec4Array> c = new osg::Vec4Array;
+	geom->setColorArray(c.get());
+	geom->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+	c->push_back(osg::Vec4(1.f, 0.f, 0.f, 1.f));
+	c->push_back(osg::Vec4(0.f, 1.f, 0.f, 1.f));
+	c->push_back(osg::Vec4(0.f, 0.f, 1.f, 1.f));
+	c->push_back(osg::Vec4(1.f, 1.f, 1.f, 1.f));
+
+	// 为唯一的法线创建一个数组
+	osg::ref_ptr<osg::Vec3Array> n = new osg::Vec3Array;
+	geom->setNormalArray(n.get());
+	geom->setNormalBinding(osg::Geometry::BIND_OVERALL);
+	n->push_back(osg::Vec3(0.f, -1.f, 0.f));
+
+	// 由保存的数据绘制四个顶点的多边形
+	geom->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::QUADS, 0, 4));
+
+	return geom;
+}
+osg::ref_ptr<osg::Node> createSceneGraph3()
+{
+	// 创建根节点
+	osg::ref_ptr<osg::Group> root = new osg::Group;
+	{
+		// 在根节点的StateSet中禁止光照
+		// 使用PROECTED以保证这一修改不会被osgviewer覆盖
+		osg::StateSet* state = root->getOrCreateStateSet();
+		state->setMode(GL_LIGHTING,
+			osg::StateAttribute::OFF |
+			osg::StateAttribute::PROTECTED);
+	}
+
+	// 创建Geode叶节点并关联Drawable
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	osg::Drawable* drawableTemp = new osg::Drawable();
+	geode->addDrawable(createDrawable().get());
+	osg::Matrix m;
+	{
+		// 左上角：使用缺省属性渲染几何体
+		osg::ref_ptr<osg::MatrixTransform> mt = new osg::MatrixTransform;
+		m.makeTranslate(-2.f, 0.f, 2.f);
+		mt->setMatrix(m);
+		root->addChild(mt.get());
+		mt->addChild(geode.get());
+
+	}
+
+	{
+		// 右上角：设置着色器为FLAT（单色）
+		osg::ref_ptr<osg::MatrixTransform> mt = new osg::MatrixTransform;
+		m.makeTranslate(2.f, 0.f, 2.f);
+		mt->setMatrix(m);
+		root->addChild(mt.get());
+		mt->addChild(geode.get());
+		osg::StateSet* state = mt->getOrCreateStateSet();
+		osg::ShadeModel* sm = new osg::ShadeModel();
+		sm->setMode(osg::ShadeModel::FLAT);
+		state->setAttribute(sm);
+	}
+
+	{
+		// 左下角：开启背面剔除
+		osg::ref_ptr<osg::MatrixTransform> mt = new osg::MatrixTransform;
+		m.makeTranslate(-2.f, 0.f, -2.f);
+		mt->setMatrix(m);
+		root->addChild(mt.get());
+		mt->addChild(geode.get());
+		osg::StateSet* state = mt->getOrCreateStateSet();
+		osg::CullFace* cf = new osg::CullFace();// 缺省值为BACK
+		state->setAttributeAndModes(cf);
+	}
+
+	{
+		// 右下角：设置多边形填充模式LINE（线框）
+		osg::ref_ptr<osg::MatrixTransform> mt = new osg::MatrixTransform;
+		m.makeTranslate(2.0f, 0.f, -2.f);
+		mt->setMatrix(m);
+		root->addChild(mt.get());
+		mt->addChild(geode.get());
+		osg::StateSet* state = mt->getOrCreateStateSet();
+		osg::PolygonMode* pm = new osg::PolygonMode(osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::LINE);
+		state->setAttributeAndModes(pm);
+
+		// 同时还设置线宽为3
+		osg::LineWidth* lw = new osg::LineWidth(3.f);
+		state->setAttribute(lw);
+	}
+
+	return root.get();
+}
+
+// 保存场景图形为.osg文件
+void saveAsOSGFile(osg::ref_ptr<osg::Node> root)
+{
+	if (!root.valid())
+	{
+		osg::notify(osg::FATAL) << "Failed in createSceneGraph()." << std::endl;
+	}
+
+	bool result = osgDB::writeNodeFile(*(root.get()), "Simple.osg");
+
+	if (!result)
+	{
+		osg::notify(osg::FATAL) << "Failed in osgDB::writeNode()." << std::endl;
+	}
+}
+
 int main()
 {
 	osg::ref_ptr<osgViewer::Viewer> viewer = new osgViewer::Viewer;
@@ -74,21 +202,5 @@ int main()
 
 	viewer->setSceneData(createSceneGraph2());
 
-	//return viewer->run();
-
-	osg::ref_ptr<osg::Node> root = createSceneGraph2();
-	if (!root.valid())
-	{
-		osg::notify(osg::FATAL) << "Failed in createSceneGraph()." << std::endl;
-	}
-
-	bool result = osgDB::writeNodeFile(*(root.get()),"Simple.osg");
-
-	if (!result)
-	{
-		osg::notify(osg::FATAL) << "Failed in osgDB::writeNode()." << std::endl;
-	}
-
-	//return 0;
 	return viewer->run();
 }
