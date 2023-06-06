be9ac6ee95859af70ab77f7e4f57013cb66a02de
无名 无姓 <lsj2543075618@gmail.com>
2023-06-06 17:28:11 +0800

    测试绘制不同类型的图元；测试纹理贴图到矩形图元上

--- source/OSGlearn.cpp
+++ source/OSGlearn.cpp
@@ -21,7 +21,7 @@
 #include <common.h>
 
 //b站《2021版OSG教学》课程源码学习
-#define DAY  2
+#define DAY  3
 
 #if 0 //自定义学习
 // 绘制地球
@@ -217,7 +217,10 @@ int main(int argc, char** argv)
 }
 #endif
 
-#if DAY == 2
+#if DAY == 2 
+//学习矩阵变换节点的使用：缩放scale，旋转rotate，平移translate（都是osg::Matrix的静态函数）
+//osg::ref_ptr<osg::MatrixTransform> rpMTRT = new osg::MatrixTransform;
+//rpMTRT->setMatrix(osg::Matrix::scale(10, 10, 10)* osg::Matrix::rotate(osg::DegreesToRadians(90.0), osg::Vec3(0, 0, 1))* osg::Matrix::translate(50, 0, 0));
 int main(int argc, char** argv)
 {
 	osg::ref_ptr<osgViewer::Viewer> rpViewer = new osgViewer::Viewer;
@@ -267,4 +270,96 @@ int main(int argc, char** argv)
 #endif
 
 #if DAY == 3
+//学习不同图元的绘制
+int main(int argc, char** argv)
+{
+	// 创建一个用于保存几何信息的对象
+	osg::ref_ptr<osg::Geometry> geom = new osg::Geometry;
+
+	// 创建四个顶点的数组
+	osg::ref_ptr<osg::Vec3Array> v = new osg::Vec3Array;
+	geom->setVertexArray(v.get());// 注意，这里v是指针，但是用“.”来使用get()函数返回指针
+	v->push_back(osg::Vec3(-1.f, 0.f, -1.f));
+	v->push_back(osg::Vec3(-0.5f, 0.f, -1.f));
+	v->push_back(osg::Vec3(-0.5f, 0.f, 1.f));
+	v->push_back(osg::Vec3(-1.f, 0.f, 1.f));
+
+	v->push_back(osg::Vec3(0.5f, 0.f, -1.f));
+	v->push_back(osg::Vec3(1.f, 0.f, -1.f));
+	v->push_back(osg::Vec3(1.f, 0.f, 1.f));
+	v->push_back(osg::Vec3(0.5f, 0.f, 1.f));
+
+
+	// 创建四种颜色的数组
+	osg::ref_ptr<osg::Vec4Array> c = new osg::Vec4Array;
+	geom->setColorArray(c.get());
+	geom->setColorBinding(osg::Geometry::BIND_OVERALL);
+	c->push_back(osg::Vec4(1.f, 0.f, 0.f, 1.f));
+
+	// 为唯一的法线创建一个数组
+	osg::ref_ptr<osg::Vec3Array> n = new osg::Vec3Array;
+	geom->setNormalArray(n.get());
+	geom->setNormalBinding(osg::Geometry::BIND_OVERALL);
+	n->push_back(osg::Vec3(0.f, -1.f, 0.f));
+
+	// 使用DrawArrays方式创建图元
+	////POINTS
+	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::POINTS, 0, v->size());
+	////LINES
+	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::LINES, 0, v->size());
+ 	////LINE_STRIP
+ 	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::LINE_STRIP, 0, v->size());
+	////LINE_LOOP
+	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::LINE_LOOP, 0, v->size());
+	////TRIANGLES：使用新的顶点绘制三角形，点数不够绘制出新的三角形，则不进行绘制
+	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::TRIANGLES, 0, v->size());
+	////TRIANGLE_STRIP：三角形带，每次绘制一个三角形舍弃一个顶点，其余顶点循环使用
+	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::TRIANGLE_STRIP, 0, v->size());
+	////TRIANGLE_FAN：第一个顶点不变，舍弃一个中间顶点，循环使用一个最后顶点
+	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::TRIANGLE_FAN, 0, v->size());
+	//QUADS：
+	osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::QUADS, 0, v->size());
+	////QUAD_STRIP：两点成线构成矢量，另外两个点构成的矢量方向要与上两点的矢量方向基本一致，否则矩形发送扭曲，类似卷轴对扭的情况
+	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::QUAD_STRIP, 0, v->size());
+	////POLYGON：(填充与线框模式不符)在OpenGL中，GL_POLYGON图元按照逆时针顺序绘制多边形的顶点。不按照逆时针多边形顶点会影响到[多边形的面向]，[多边形的填充]，[深度测试和遮挡关系]
+	//osg::ref_ptr<osg::DrawArrays> drawArrays = new osg::DrawArrays(osg::PrimitiveSet::POLYGON, 0, v->size());
+	geom->addPrimitiveSet(drawArrays.get());
+
+
+	{//设置geom的纹理属性：图元进行贴图//在默认情况下，最终效果是顶点颜色会与纹理混合
+		// 创建纹理坐标数组
+		osg::ref_ptr<osg::Vec2Array> texCoords = new osg::Vec2Array;
+		//纹理坐标对应第一个矩形
+		texCoords->push_back(osg::Vec2(0.0, 0.0));   // 左下角纹理坐标
+		texCoords->push_back(osg::Vec2(1.0, 0.0));   // 右下角纹理坐标
+		texCoords->push_back(osg::Vec2(1.0, 1.0));   // 右上角纹理坐标
+		texCoords->push_back(osg::Vec2(0.0, 1.0));   // 左上角纹理坐标
+		//纹理坐标对应第二个矩形
+		texCoords->push_back(osg::Vec2(0.0, 0.0));   // 左下角纹理坐标
+		texCoords->push_back(osg::Vec2(1.0, 0.0));   // 右下角纹理坐标
+		texCoords->push_back(osg::Vec2(1.0, 1.0));   // 右上角纹理坐标
+		texCoords->push_back(osg::Vec2(0.0, 1.0));   // 左上角纹理坐标
+		geom->setTexCoordArray(0, texCoords.get());//geom设置纹理坐标
+
+		//创建纹理对象
+		osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D;
+		osg::ref_ptr<osg::Image> image = osgDB::readImageFile("C:/Users/袁壮/Desktop/测试/myTest.jpg");
+		texture->setImage(image.get());
+
+		//创建状态集，并将纹理对象设置为当前状态的纹理单元0
+		osg::ref_ptr<osg::StateSet> stateSet = geom->getOrCreateStateSet();
+		stateSet->setTextureAttributeAndModes(0, texture.get());
+	}
+	
+	// 向Geode类添加几何体（Drawable）并返回Geode
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	geode->addDrawable(geom.get());
+
+	osg::ref_ptr<osgViewer::Viewer> rpViewer = new osgViewer::Viewer;
+	rpViewer->setSceneData(geode.get());
+	rpViewer->addEventHandler(new osgViewer::StatsHandler);
+	rpViewer->addEventHandler(new osgViewer::WindowSizeHandler);
+	rpViewer->addEventHandler(new osgGA::StateSetManipulator(rpViewer->getCamera()->getOrCreateStateSet()));
+	return rpViewer->run();
+}
 #endif
