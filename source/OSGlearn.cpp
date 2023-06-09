a111b5b57bd5204d81c20b876711371a8a9006d7
无名 无姓 <lsj2543075618@gmail.com>
2023-06-09 11:06:01 +0800

    添加学习场景：几何体的更新回调函数的写法

--- source/OSGlearn.cpp
+++ source/OSGlearn.cpp
@@ -772,7 +772,7 @@ int main(int argc, char** argv)
 #endif
 
 
-#if 1
+#if 0
 //#知识点：构建osg::Geometry对象进行绘制图形
 //测试场景：通过创建顶点数组、颜色数组、法线数组和纹理数组，构造一个简单的人字顶房屋
 
@@ -889,3 +889,73 @@ int main(int argc, char** argv)
 }
 
 #endif
+
+#if 1
+//#知识点：几何体的更新回调函数的写法，在绘图函数内修改顶点数组
+//#测试场景：利用几何体的更新回调函数修改顶点数组，是的线图元进行环状运动
+
+/* -*-c++-*- Copyright (C) 2009 Wang Rui <wangray84 at gmail dot com>
+ * OpenSceneGraph Engine Book - Design and Implementation
+ * How to create a dynamic line
+*/
+
+#include <osg/LineWidth>
+#include <osg/Geometry>
+#include <osg/Geode>
+#include <osgViewer/Viewer>
+
+class DynamicLineCallback : public osg::Drawable::UpdateCallback
+{
+public:
+	DynamicLineCallback() : _angle(0.0) {}
+
+	virtual void update(osg::NodeVisitor* nv, osg::Drawable* drawable)
+	{
+		osg::Geometry* geom = dynamic_cast<osg::Geometry*>(drawable);
+		if (!geom) return;
+
+		osg::Vec3Array* vertices = dynamic_cast<osg::Vec3Array*>(geom->getVertexArray());
+		if (vertices)
+		{
+			for (osg::Vec3Array::iterator itr = vertices->begin(); itr != vertices->end() - 1; ++itr)
+				itr->set((*(itr + 1)));//修改顶点位置，将最后一个顶点做圆周运动，其它点取代其下一个点的位置，实现跟这最后一个点进行运动的效果
+
+			_angle += 1.0 / 10.0;
+			osg::Vec3& pt = vertices->back();
+			pt.set(10.0 * cos(_angle), 0.0, 10.0 * sin(_angle));
+			vertices->dirty();
+		}
+	}
+
+protected:
+	float _angle;
+};
+
+int main(int argc, char** argv)
+{
+	osg::ref_ptr<osg::Vec3Array> vertices = new osg::Vec3Array(10);
+	for (unsigned int i = 0; i < 10; ++i)
+		(*vertices)[i].set(float(i), 0.0, 0.0);
+
+	osg::ref_ptr<osg::Geometry> lineGeom = new osg::Geometry;
+	lineGeom->setVertexArray(vertices.get());
+	lineGeom->addPrimitiveSet(new osg::DrawArrays(osg::DrawArrays::LINE_STRIP, 0, 10));
+
+	lineGeom->setInitialBound(osg::BoundingBox(osg::Vec3(-10.0, -10.0, -10.0), osg::Vec3(10.0, 10.0, 10.0)));//节点的boundingbox就是viewer的展示区域，viewer自动对场景节点的boundingbox进行全屏显示
+	lineGeom->setUpdateCallback(new DynamicLineCallback);
+	lineGeom->setUseDisplayList(false);//显示列表是编译好的程序指令，默认是开启的，顶点数组进行修改时，显示列表无法进行修改，所以需要禁用，在更新回调中，需要启用VBO
+	lineGeom->setUseVertexBufferObjects(true);
+
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	geode->getOrCreateStateSet()->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
+	geode->getOrCreateStateSet()->setAttribute(new osg::LineWidth(2.0));
+	geode->addDrawable(lineGeom.get());
+
+	osgViewer::Viewer viewer;
+	viewer.setSceneData(geode.get());
+	return viewer.run();
+}
+
+#endif
+
+
