2ae2f9e7f289f6434e62a63c582d0ccf0cbd60e2
无名 无姓 <lsj2543075618@gmail.com>
2023-06-09 16:44:01 +0800

    添加学习场景：绘制文字

--- source/OSGlearn.cpp
+++ source/OSGlearn.cpp
@@ -959,7 +959,7 @@ int main(int argc, char** argv)
 #endif
 
 
-#if 1
+#if 0
 
 //#知识点：绘制位图图像，绘制的位图不是能够旋转的几何体，所以用户观察角度的变化不会影响位图的朝向
 
@@ -1001,4 +1001,74 @@ int main(int argc, char** argv)
 
 #endif
 
+
+#if 1
+
+//#知识点：绘制文字，包括文字字体、大小、颜色和内容的设置，以及将中文字符转换到宽字符集的方法
+
+/* -*-c++-*- Copyright (C) 2009 Wang Rui <wangray84 at gmail dot com>
+ * OpenSceneGraph Engine Book - Design and Implementation
+ * How to write a poem
+*/
+
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osgText/Text>
+#include <osgViewer/Viewer>
+#include <locale.h>
+
+void setupProperties(osgText::Text& textObject, osgText::Font* font,
+	float size, const osg::Vec3& pos)
+{
+	textObject.setFont(font);
+	textObject.setCharacterSize(size);
+	textObject.setPosition(pos);
+	textObject.setColor(osg::Vec4(0.0, 0.0, 1.0, 1.0));
+	textObject.setAlignment(osgText::Text::CENTER_BOTTOM);
+	textObject.setAxisAlignment(osgText::Text::XZ_PLANE);
+}
+
+void createContent(osgText::Text& textObject, const char* string)
+{
+	int requiredSize = mbstowcs(NULL, string, 0);
+	wchar_t* wtext = new wchar_t[requiredSize + 1];
+	mbstowcs(wtext, string, requiredSize + 1);
+	textObject.setText(wtext);
+	delete wtext;
+}
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, ".936");
+	const char* titleString = "木兰辞\n拟古决绝词柬友";
+	const char* textString = {
+		"人生若只如初见，何事秋风悲画扇；\n"
+		"等闲变却故人心，却道故人心易变。\n"
+		"骊山语罢清宵半，夜雨霖铃终不怨；\n"
+		"何如薄幸锦衣郎，比翼连枝当日愿。"
+	};
+
+	osgText::Font* fontHei = osgText::readFontFile("Fonts/simhei.ttf");
+	osgText::Font* fontKai = osgText::readFontFile("Fonts/simkai.ttf");
+
+	osg::ref_ptr<osgText::Text> title = new osgText::Text;
+	setupProperties(*title, fontHei, 20.0f, osg::Vec3(0.0f, 0.0f, 0.0f));
+	createContent(*title, titleString);
+
+	osg::ref_ptr<osgText::Text> text = new osgText::Text;
+	setupProperties(*text, fontKai, 15.0f, osg::Vec3(0.0f, 0.0f, -80.0f));
+	createContent(*text, textString);
+
+	osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+	geode->addDrawable(osg::createTexturedQuadGeometry(
+		osg::Vec3(-150.0, 1.0, -130.0), osg::Vec3(300.0, 0.0, 0.0), osg::Vec3(0.0, 0.0, 200.0), 1.0, 1.0));
+	geode->addDrawable(title.get());
+	geode->addDrawable(text.get());
+
+	osgViewer::Viewer viewer;
+	viewer.setSceneData(geode.get());
+	return viewer.run();
+}
+
+#endif
 
