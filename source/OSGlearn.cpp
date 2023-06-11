0b5e1856dc22705864f158116c911fc49291482c
无名 无姓 <lsj2543075618@gmail.com>
2023-06-11 21:29:51 +0800

    添加学习场景：创建多视景器类osgViewer::CompositeViewer的多窗口显示，每个窗口都有各自的场景数据

--- source/OSGlearn.cpp
+++ source/OSGlearn.cpp
@@ -1207,8 +1207,8 @@ int main(int argc, char** argv)
 
 #endif
 
-#if 1
-//#知识点：创建单视景器的多个窗口显示（通过设置相机来实现）
+#if 0
+//#知识点：创建单视景器类osgViewer::Viewer的多个窗口显示（通过设置相机来实现），多个窗口共享同一个场景数据
 
 
 /* -*-c++-*- Copyright (C) 2009 Wang Rui <wangray84 at gmail dot com>
@@ -1259,6 +1259,40 @@ int main(int argc, char** argv)
  	viewer.addSlave(createCamera(505, 405, 400, 300), osg::Matrixd::translate(-1.0, 1.0, 0.0), osg::Matrixd());
 	viewer.setSceneData(model);
 	return viewer.run();
+}
+
+#endif
+
+#if 1
+//#知识点：创建多视景器类osgViewer::CompositeViewer的多窗口显示，每个窗口都有各自的场景数据
+/* -*-c++-*- Copyright (C) 2009 Wang Rui <wangray84 at gmail dot com>
+ * OpenSceneGraph Engine Book - Design and Implementation
+ * How to create a compsite viewer
+*/
+
+#include <osg/Group>
+#include <osgDB/ReadFile>
+#include <osgViewer/CompositeViewer>
+
+int main(int argc, char** argv)
+{
+	osg::ref_ptr<osgViewer::View> view1 = new osgViewer::View;
+	view1->setUpViewInWindow(0, 50, 320, 240);
+	view1->setSceneData(osgDB::readNodeFile("cow.osg"));
+
+	osg::ref_ptr<osgViewer::View> view2 = new osgViewer::View;
+	view2->setUpViewInWindow(320, 50, 320, 240);
+	view2->setSceneData(osgDB::readNodeFile("cessna.osg"));
+
+	osg::ref_ptr<osgViewer::View> view3 = new osgViewer::View;
+	view3->setUpViewInWindow(640, 50, 320, 240);
+	view3->setSceneData(osgDB::readNodeFile("axes.osg"));
+
+	osgViewer::CompositeViewer compositeViewer;
+	compositeViewer.addView(view1.get());
+	compositeViewer.addView(view2.get());
+	compositeViewer.addView(view3.get());
+	return compositeViewer.run();
 }
 
 #endif
